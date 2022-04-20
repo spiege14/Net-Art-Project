6ef9c6f694741648871891a9f4e39e75b47bb9b8
spiege14 <103605992+spiege14@users.noreply.github.com>
2022-04-20 00:18:13 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  rect(80,215,40,75)
+  circle(100,200,75)
+  ellipse(115,200,10,20)
+  ellipse(85,200,10,20)
+  circle(100,210,5)
+  line(80,290,40,330);
+  line(120,290,160,330)
+}
